e743efcd74376a9cab03ab16b9161439edd99828
Emilio J. Gomez <moleculax@gmail.com>
2026-06-10 13:23:04 -0300

    Continuamos

--- jpa-app/src/main/java/com/debugapp/jpa_app/JpaAppApplication.java
+++ jpa-app/src/main/java/com/debugapp/jpa_app/JpaAppApplication.java
@@ -1,20 +1,17 @@
 package com.debugapp.jpa_app;
 
-import com.debugapp.jpa_app.entity.OrderEntity;
-import com.debugapp.jpa_app.repository.OrderRepository;
+import com.debugapp.jpa_app.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
-
 @SpringBootApplication
 public class JpaAppApplication implements CommandLineRunner {
 
 	@Autowired
-	private OrderRepository orderRepository;
-
+//	private OrderRepository orderRepository;
+	private OrderService orderService;
 	public static void main(String[] args) {
 
 		SpringApplication.run(JpaAppApplication.class, args);
@@ -22,28 +19,8 @@ public class JpaAppApplication implements CommandLineRunner {
 	}
 
 	@Override
-	//public  void run(String[] args) throws Exception {
-	public  void run(String... args) throws Exception {
-
-		// Long orderId = 16L; // Reemplaza con el ID que deseas buscar
-		//orderRepository.findById(orderId).ifPresent(order -> System.out.println("Cliente: " + order.getClientName()));
-//		orderRepository.findAll().forEach(order -> System.out.println(order.getId()+ " / " +order.getClientName()
-//		+ " / " + order.getBill().getId() + " / " + order.getBill().getTotalAmount() + " / " + order.getBill().getRfc())
-//		);
-//		System.out.println("=== Fin de la consulta ===");
-
-
-		// El método findAll() de CrudRepository retorna Iterable<T>, no List<T>.
-		//  Por lo tanto, no puedes asignar directamente el resultado a una variable
-		//  de tipo List<OrderEntity>. En su lugar, puedes usar un bucle para iterar sobre
-		//  el Iterable o convertirlo a una lista si lo necesitas.
-		Iterable<OrderEntity> resultado = orderRepository.findAll();
-		//System.out.println(resultado);
-		resultado.forEach(order -> System.out.println(order.getClientName()
-				+ " / " + order.getBill().getId()
-				+ " / " + order.getBill().getTotalAmount()
-				+ " / " + order.getBill().getRfc()));
-
+	public void run(String... args) throws Exception {
+		orderService.run(args);
 	}
 
 }
---
+++ jpa-app/src/main/java/com/debugapp/jpa_app/service/OrderService.java
@@ -0,0 +1,45 @@
+package com.debugapp.jpa_app.service;
+
+import com.debugapp.jpa_app.entity.OrderEntity;
+import com.debugapp.jpa_app.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    //public  void run(String[] args) throws Exception {
+    public  void run(String... args) throws Exception {
+
+        // Long orderId = 16L; // Reemplaza con el ID que deseas buscar
+        //orderRepository.findById(orderId).ifPresent(order -> System.out.println("Cliente: " + order.getClientName()));
+//		orderRepository.findAll().forEach(order -> System.out.println(order.getId()+ " / " +order.getClientName()
+//		+ " / " + order.getBill().getId() + " / " + order.getBill().getTotalAmount() + " / " + order.getBill().getRfc())
+//		);
+//		System.out.println("=== Fin de la consulta ===");
+
+
+        // El método findAll() de CrudRepository retorna Iterable<T>, no List<T>.
+        //  Por lo tanto, no puedes asignar directamente el resultado a una variable
+        //  de tipo List<OrderEntity>. En su lugar, puedes usar un bucle para iterar sobre
+        //  el Iterable o convertirlo a una lista si lo necesitas.
+        Iterable<OrderEntity> resultado = orderRepository.findAll();
+        //System.out.println(resultado);
+        resultado.forEach(order -> {
+            if (order.getBill() != null) {
+                System.out.println(order.getClientName()
+                        + " / " + order.getBill().getId()
+                        + " / " + order.getBill().getTotalAmount()
+                        + " / " + order.getBill().getRfc());
+            } else {
+                System.out.println(order.getClientName() + " / SIN FACTURA");
+            }
+        });
+
+    }
+
+
+}
